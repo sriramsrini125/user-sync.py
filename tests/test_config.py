5bd48f09178d301b138e4a154775bf7206d26583
vossen <danimaejanssen@gmail.com>
2019-11-11 16:19:26 -0600

    current pass

--- tests/test_config.py
+++ tests/test_config.py
@@ -204,7 +204,7 @@ def test_get_directory_connector_configs(cli_args, config_files):
 
     # Test method to verify 'okta', 'csv', 'ldap' are in the accessed_keys set
     result = config_loader.main_config.child_configs.get('directory_users').child_configs['connectors'].accessed_keys
-    assert result == {'okta', 'csv', 'ldap'}
+    assert result == {'csv', 'okta', 'adobe_console', 'ldap'}
 
 
 def test_get_directory_connector_module_name(cli_args, config_files):
